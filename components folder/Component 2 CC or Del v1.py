7e61737f82b6318edcf76df1a70b0694d3cb36af
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-03-31 09:53:20 +1300

    Added choice input

--- components folder/Component 2 CC or Del v1.py
+++ components folder/Component 2 CC or Del v1.py
@@ -1,3 +1,10 @@
 print("Would you like to Click and collect your order or want your order to be Delivered?")
-print("Please enter 1 for Click and collect")
-print("Please enter 2 for Delivery")
+print("Enter 1 for Click and collect")
+print("Enter 2 for Delivery")
+choice = int(input("Please enter 1 or 2: "))
+if choice == 1:
+    print("Click and collect")
+elif choice == 2:
+    print("Delivery")
+else:
+    print("Error invalid input")
