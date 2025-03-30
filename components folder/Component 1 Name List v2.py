53d2c3514953305d70aa45ec55076c8fbfafdd14
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-03-31 09:32:39 +1300

    ansi escape codes for bold text

--- components folder/Component 1 Name List v2.py
+++ components folder/Component 1 Name List v2.py
@@ -1,13 +1,18 @@
+# ANSI escape codes for bold text
+bold = "\033[1m"
+reset = "\033[0m"
+
 import random
 from random import randint
 
 bot_names = ["Elrick", "Mateo", "Edward", "River", "Kyle",  "Sean Combs", "Faustino", "Gallegos"]
 
+
 def welcome():
     num = randint(0,7)
     name = (bot_names[num])
-    print("***Welcome to Jollibee ***")
-    print("***My name is",name,"***")
-    print("***I will be here to help you order your delicious Jollibee meal***")    
-
+    print(f"{bold}***Welcome to Jollibee***{reset}")
+    print(f"{bold}***My name is {name}***{reset}")
+    print(f"{bold}***I will be here to help you order your delicious Jollibee meal***{reset}")
+    print("test")
 welcome()
