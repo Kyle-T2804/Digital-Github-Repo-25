7e978df8559fea08838d16d7982198a2dbca17d1
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-03-31 09:28:09 +1300

    2nd version

---
+++ components folder/Component 1 Name List v2.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+bot_names = ["Elrick", "Mateo", "Edward", "River", "Kyle",  "Sean Combs", "Faustino", "Gallegos"]
+
+def welcome():
+    num = randint(0,7)
+    name = (bot_names[num])
+    print("***Welcome to Jollibee ***")
+    print("***My name is",name,"***")
+    print("***I will be here to help you order your delicious Jollibee meal***")    
+
+welcome()
