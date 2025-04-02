18094b0d42d10cbd362e9df7748d695181535ece
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-04-03 11:33:07 +1300

    new version

---
+++ components folder/Component 2 CC or Del v2.py
@@ -0,0 +1,10 @@
+print("Would you like to Click and collect your order or want your order to be Delivered?")
+print("Enter 1 for Click and collect")
+print("Enter 2 for Delivery")
+choice = int(input("Please enter 1 or 2: "))
+if choice == 1:
+    print("Click and collect")
+elif choice == 2:
+    print("Delivery")
+else:
+    print("Error invalid input!")
