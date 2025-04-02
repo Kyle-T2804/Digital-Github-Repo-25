49fe6390017c426ff5408e0da6c9ae0e152e1827
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-04-03 11:58:25 +1300

    fixed stuff

---
+++ components folder/Component 2 CC or Del v3.py
@@ -0,0 +1,21 @@
+low = 1
+high = 2
+num = 0
+
+print("Would you like to Click and collect your order or want your order to be Delivered?")
+print("Enter 1 for Click and collect")
+print("Enter 2 for Delivery")
+while True:
+    try:
+        num = int(input("Please enter 1 or 2: "))
+        if num == 1:
+            print("Click and collect")
+            break
+        elif num == 2:
+            print("Delivery")
+            break
+        else: num = int(input("Invalid number, please enter 1 or 2: "))
+    except ValueError:
+        print("Invalid input, please enter the options between 1 or 2")
+
+print("Continue")
