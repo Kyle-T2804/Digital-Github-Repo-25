3580e00765a41a472776ad560df70ee3bd5c5df3
Kyle-T2804 <22190@my.sanctamarias.school.nz>
2025-05-06 09:53:50 +1200

    fixed

--- components folder/Component 2 CC or Del v4.py
+++ components folder/Component 2 CC or Del v4.py
@@ -7,13 +7,16 @@ print("Enter 2 for Delivery")
 while True:
     try:
         num = int(input("Please enter 1 or 2: "))
-        if num == LOW:
-            print("Click and collect")
-            break
-        elif num == HIGH:
-            print("Delivery")
-            break
+        if num >= LOW and num <= HIGH:
+            if num == 1:
+                print("Click and collect")
+                break
+        elif num == 2:
+                print("Delivery")
+                break
         else: num = int(input("Invalid number, please enter 1 or 2: "))
+    
+    
     except ValueError:
         print("Invalid input, please enter the options between 1 or 2")
 
