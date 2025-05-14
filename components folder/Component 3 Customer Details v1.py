5b78985ca3bdbd4651dd262da3e2bd0878b1de82
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-05-15 11:49:27 +1200

    New components

--- components folder/Component 3 Customer Details v1.py
+++ components folder/Component 3 Customer Details v1.py
@@ -1,12 +1,13 @@
+# ask user for input
+print("Please enter your name: ")
+name = input()
+print("Please enter your phone number: ")
+phone = input()
+print("Please enter your adress: ")
+adress = input()
 
-order_type = input("Is your order for pickup or delivery? ").lower()
 
-if order_type == "pickup":
-    name = input("Enter your name: ")
-    phone = input("Enter your phone number: ")
-    print("Thanks", name, "we'll text you when your order is ready.")
-elif order_type == "delivery":
-    name = input("Enter your name: ")
-    address = input("Enter your address: ")
-    phone = input("Enter your phone number: ")
-    print("Thanks", name)
+#printing customer details
+print(name)
+print(phone)
+print(adress)
---
+++ components folder/Component 3 Customer Details v2.py
@@ -0,0 +1,10 @@
+#Customer details dictionary
+customer_details = {}
+
+
+#while loop for validation of name
+
+customer_details["name"] = input("Please enter your name: ")
+customer_details["phone"] = input("Please enter your phone number: ")
+
+print(customer_details)
