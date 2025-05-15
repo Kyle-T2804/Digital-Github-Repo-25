a4408d0ce31426694f3c754d8ebb0debcce612d7
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-05-15 12:05:35 +1200

    Fixed version 3 of Phone number Validation

--- components folder/Phone number Validation v3.py
+++ components folder/Phone number Validation v3.py
@@ -5,13 +5,13 @@ customer_details = {}
 #regular phone expression pattern for phone validation
 pattern = r"^\d{8,12}$"
 #while loop for validation of name
-
-phone_number = input("Please enter your phone number: ")
-if re.match(pattern, phone_number):
-    customer_details["phone"] = phone_number
-else:
-    print("This is an invalid phone number")
-    
-#customer_details["phone"] = input("Please enter your phone number: ")
+while True:
+    phone_number = input("Please enter your phone number: ")
+    if re.match(pattern, phone_number):
+        customer_details["phone"] = phone_number
+        break
+    else:
+        print("This is an invalid phone number")
+        
 
 print(customer_details)
